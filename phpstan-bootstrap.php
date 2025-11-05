79dc400b02a73ca7fc965794d0a65381bc8800ed
homeextent <jeremy@theinnovativegroup.ca>
2025-11-05 09:37:31 -0500

    fix(ci): load wordpress-stubs in bootstrap

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,3 +2,4 @@
 // Minimal bootstrap for static analysis
 define('ABSPATH', __DIR__ . '/'); // silence some WP checks
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';

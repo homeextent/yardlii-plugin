b1ffc07d20133939f565fcb25bf1eff59666d166
homeextent <jeremy@theinnovativegroup.ca>
2025-11-05 09:43:01 -0500

    Update phpstan-bootstrap.php

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,4 +2,3 @@
 // Minimal bootstrap for static analysis
 define('ABSPATH', __DIR__ . '/'); // silence some WP checks
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';

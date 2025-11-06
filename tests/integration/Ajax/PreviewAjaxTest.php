8500b975117133e123ea144eb22686d4acf5a24e
homeextent <jeremy@theinnovativegroup.ca>
2025-11-06 17:47:45 -0500

    test(integration): add happy path for preview email

--- tests/integration/Ajax/PreviewAjaxTest.php
+++ tests/integration/Ajax/PreviewAjaxTest.php
@@ -67,4 +67,46 @@ final class PreviewAjaxTest extends WP_Ajax_UnitTestCase
             );
         }
     }
+    /**
+     * Test: The endpoint must succeed and return merged HTML for an authorized user.
+     */
+    publicF function test_admin_with_valid_data_should_succeed(): void
+    {
+        // 1. Create an admin user and log in.
+        $admin_id = self::factory()->user->create(['role' => 'administrator']);
+        wp_set_current_user($admin_id);
+
+        // 2. Set the expected $_POST data for the handler.
+        $_POST['_ajax_nonce'] = wp_create_nonce('yardlii_tv_preview');
+        $_POST['form_id'] = 'test_form_456';
+        $_POST['email_type'] = 'reject';
+        // We will send unsaved overrides from the UI
+        $_POST['subject'] = 'Preview Subject {{site.name}}';
+        $_POST['body'] = '<p>Hello {{user.display_name}}, this is a test.</p>';
+
+        try {
+            // 3. Run the handler
+            $this->_handleAjax($this->ajax_action);
+
+            $this->fail('AJAX handler did not die as expected.');
+        } catch (\WPAjaxDieStopException $e) {
+            // 4. We expect a JSON success response.
+            $data = json_decode($e->getMessage(), true);
+
+            $this->assertIsArray($data);
+            $this->assertTrue($data['success']);
+            $this->assertArrayHasKey('html', $data['data']);
+
+            // 5. Assert the HTML contains the merged subject and body.
+            // The PreviewAjax handler prepends the subject to the HTML.
+            $this->assertStringContainsString(
+                'Preview Subject My Test Blog', // '{{site.name}}' becomes 'My Test Blog'
+                $data['data']['html']
+            );
+            $this->assertStringContainsString(
+                '<p>Hello administrator, this is a test.</p>', // '{{user.display_name}}' becomes 'administrator'
+                $data['data']['html']
+            );
+        }
+    }
 }

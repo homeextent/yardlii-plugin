c4e80333d7aff42a6ac13fe53554deb0ed42a153
homeextent <jeremy@theinnovativegroup.ca>
2025-11-06 16:50:34 -0500

    Create AjaxHistoryAjaxTest.php

---
+++ tests/integration/AjaxHistoryAjaxTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Yardlii\Tests\Integration\Ajax;
+
+use WP_Ajax_UnitTestCase;
+use Yardlii\Core\Features\TrustVerification\Caps; // We'll need this for later tests
+
+/**
+ * Tests for the 'yardlii_tv_history_load' AJAX action.
+ * @covers \Yardlii\Core\Features\TrustVerification\Requests\HistoryAjax
+ */
+final class HistoryAjaxTest extends WP_Ajax_UnitTestCase
+{
+    /**
+     * The AJAX action hook to test.
+     * @var string
+     */
+    protected string $ajax_action = 'yardlii_tv_history_load';
+
+    /**
+     * Test: The endpoint must fail with a 403 error if no nonce is provided.
+     *
+     * This corresponds to the check_ajax_referer('yardlii_tv_history', ...)
+     * in the handle() method.
+     */
+    public function test_missing_nonce_should_fail(): void
+    {
+        try {
+            // This runs the AJAX action handler
+            $this->_handleAjax($this->ajax_action);
+
+            // If the line above doesn't 'die', the test failed.
+            $this->fail('AJAX handler did not die as expected.');
+        } catch (\WPAjaxDieStopException $e) {
+            // This is the expected outcome.
+            // A 403 'Forbidden' response is the default for check_ajax_referer() failure.
+            $this->assertEquals('403', $e->getMessage());
+        }
+    }
+}

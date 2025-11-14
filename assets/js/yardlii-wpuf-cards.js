2ce4da25df4cd528ec707f0ae0134631b2993f82
homeextent <jeremy@theinnovativegroup.ca>
2025-11-13 21:44:45 -0500

    Fix: WPUF Pro styles for multistep cards and uploader (v3.9.1) (#63)

--- assets/js/yardlii-wpuf-cards.js
+++ assets/js/yardlii-wpuf-cards.js
@@ -5,27 +5,42 @@ jQuery(document).ready(function($) {
      */
     $('.wpuf-form-add').each(function() {
         var $form = $(this);
+
+        // 1. CHECK FOR MULTISTEP
+        // If this is a Multistep form, WPUF already groups fields into <fieldset> tags.
+        // Our CSS handles the styling. We do NOT want to restructure the DOM here.
+        if ($form.find('.wpuf-multistep-fieldset').length > 0) {
+            $form.addClass('yardlii-multistep-cards-active'); // Helper class for CSS
+            return; // EXIT SCRIPT
+        }
+
+        // 2. STANDARD FORM LOGIC (For single-step forms)
         var $allFields = $form.find('> li');
         
-        // If no fields or no section breaks, don't run to avoid layout breakage
+        // If no fields or no section breaks, don't run
         if ($allFields.length === 0) return;
 
-        // Create a container to hold our cards
+        // Only run if there is at least one section break to define groups
+        if ($form.find('li.section_break').length === 0) {
+            // Optional: If you want single-step forms WITHOUT breaks to still be one big card:
+            $form.wrapInner('<div class="yardlii-form-cards-wrapper"><div class="yardlii-card"><ul class="wpuf-form"></ul></div></div>');
+            return;
+        }
+
+        // Create a container
         var $wrapper = $('<div class="yardlii-form-cards-wrapper"></div>');
         
-        // Variables for loop
         var $currentCard = null;
         var $currentList = null;
 
-        // Helper to start a new card
         function createCard() {
             var $card = $('<div class="yardlii-card"></div>');
-            var $ul = $('<ul class="wpuf-form"></ul>'); // Maintain class for WPUF CSS compatibility
+            var $ul = $('<ul class="wpuf-form"></ul>'); 
             $card.append($ul);
             return { card: $card, list: $ul };
         }
 
-        // Init first card (for fields before the first section break)
+        // Init first card
         var current = createCard();
         $currentCard = current.card;
         $currentList = current.list;
@@ -34,40 +49,28 @@ jQuery(document).ready(function($) {
         $allFields.each(function() {
             var $field = $(this);
 
-            // If it's a submit area, move it out of cards entirely (or keep in last card)
-            // Usually WPUF puts submit in `li.wpuf-submit`. We want that separate or sticky.
             if ($field.hasClass('wpuf-submit')) {
-                // We will handle submit button separately in CSS/JS if needed
-                // For now, append to the last card list
                 $currentList.append($field);
                 return;
             }
 
-            // Is this a Section Break?
             if ($field.hasClass('section_break')) {
-                // Start a new card
                 current = createCard();
                 $currentCard = current.card;
                 $currentList = current.list;
                 $wrapper.append($currentCard);
-                
-                // Add the section break (header) to the new list
                 $currentList.append($field);
             } else {
-                // Regular field, append to current list
                 $currentList.append($field);
             }
         });
 
-        // Replace original UL with our new structure
         $form.html($wrapper);
         
-        // Cleanup: Remove empty cards (if any)
         $wrapper.find('.yardlii-card').each(function() {
             if ($(this).find('li').length === 0) $(this).remove();
         });
         
-        // Trigger a layout ready event
         $(document).trigger('yardlii_cards_rendered');
     });
 });
